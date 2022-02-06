61fc405e2ee6c5ac7685cf8ca2254fcb3b1ac4d1
prashu-asp <saiprashanth0503@gmail.com>
2022-02-06 15:56:28 +0530

    5-active links

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
-class HomeNav extends Component {
-    state = {  } 
-    render() { 
-        return (
-            <div>
-                <Link to='/'>Home</Link>&nbsp;&nbsp;
-                <Link to='/blogs'>Blogs</Link>
-            </div>
-        );
+import {NavLink} from 'react-router-dom'
+//styling is done in "index.css"
+const HomeNav = () => {
+    
+
+    const navLinkStyles = ({isActive}) => {
+        return {
+              fontWeight : isActive ? 'bold' : 'normal',
+              textDecoration : isActive ? 'none' : 'underline',
+        }
+
     }
+
+    //nav link component will set a property called "isActive" which can be destructured from this function parameter
+   
+        return (
+            <nav>
+                <NavLink style={navLinkStyles}  to='/'>Home</NavLink>&nbsp;&nbsp;
+                <NavLink style={navLinkStyles} to='/blogs'>Blogs</NavLink>
+            </nav>
+        );
+    
 }
  
 export default HomeNav;
