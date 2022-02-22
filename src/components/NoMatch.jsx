9dc97830db385de8e8632622f6dad2ec958c88b7
prashu-asp <saiprashanth0503@gmail.com>
2022-02-22 07:20:08 +0530

    7.No match route

---
+++ src/components/NoMatch.jsx
@@ -0,0 +1,9 @@
+export const NoMatch = () => {
+    return(
+        <div>
+            Page Not found
+        </div>
+    )
+}
+
+//this helps such that user wont think page is still loading if we enter a woring url
