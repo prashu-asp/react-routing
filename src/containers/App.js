72eb8396c34b7f532388192e2103c323eba3208a
prashu-asp <saiprashanth0503@gmail.com>
2022-02-22 07:20:28 +0530

    7.No match route

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,7 @@ import BlogsPage from '../components/blogsPage'
 import {Routes , Route} from 'react-router-dom'
 import Home from '../components/Home';
 import {OrderSummary} from '../components/ordersummary'
+import {NoMatch} from '../components/NoMatch'
 
 class App extends React.Component {
     render() {
@@ -16,10 +17,11 @@ class App extends React.Component {
                     <Route path='/' element={<Home />}></Route>
                     <Route path='/blogs' element={<BlogsPage />}></Route>
                     <Route path='/order-summary' element={<OrderSummary />}></Route>
+                    <Route path='*' element={<NoMatch />}></Route> 
             </Routes>
             </div>
 
-           
+           //here  path='*' means when no routing matched load this component
         );
     }
 }
